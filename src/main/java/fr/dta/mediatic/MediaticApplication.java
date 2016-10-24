c829c953b3a95743b285efec114c565d7c28944e
Pierre Robbes <robbesp@gmail.com>
2016-10-24 13:52:57 +0200

    test pour jenkins

--- src/main/java/fr/dta/mediatic/MediaticApplication.java
+++ src/main/java/fr/dta/mediatic/MediaticApplication.java
@@ -10,6 +10,6 @@ public class MediaticApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MediaticApplication.class, args);
+        System.out.println("coucou");
     }
-
 }
