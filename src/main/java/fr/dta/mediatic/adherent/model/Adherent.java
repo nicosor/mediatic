0d9c03fe7cb08d5228ca26f9f91c942811ae9722
Pierre Burgain <Pierre Burgain>
2016-11-16 15:33:32 +0100

    adherent et adherent dao

--- src/main/java/fr/dta/mediatic/adherent/model/Adherent.java
+++ src/main/java/fr/dta/mediatic/adherent/model/Adherent.java
@@ -1,5 +1,179 @@
 package fr.dta.mediatic.adherent.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import fr.dta.mediatic.subscription.model.Subscription;
+
+@Entity
 public class Adherent {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String lastname;
+	private String firstname;
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+	private String adresse;
+	private int postalCode;
+	private String city;
+	@OneToOne
+	private Subscription subscription;
+	private String mail;
+	
+	public Adherent(String lastname, String firstname, Date birthday, String adresse, int postalCode, String city,
+			Subscription subscription, String mail) {
+		super();
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.birthday = birthday;
+		this.adresse = adresse;
+		this.postalCode = postalCode;
+		this.city = city;
+		this.subscription = subscription;
+		this.mail = mail;
+	}
+	
+	@Override
+	public String toString() {
+		return "Adherent [id=" + id + ", lastname=" + lastname + ", firstname=" + firstname + ", birthday=" + birthday
+				+ ", adresse=" + adresse + ", postalCode=" + postalCode + ", city=" + city + ", subscription="
+				+ subscription + ", mail=" + mail + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((mail == null) ? 0 : mail.hashCode());
+		result = prime * result + postalCode;
+		result = prime * result + ((subscription == null) ? 0 : subscription.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Adherent))
+			return false;
+		Adherent other = (Adherent) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (mail == null) {
+			if (other.mail != null)
+				return false;
+		} else if (!mail.equals(other.mail))
+			return false;
+		if (postalCode != other.postalCode)
+			return false;
+		if (subscription == null) {
+			if (other.subscription != null)
+				return false;
+		} else if (!subscription.equals(other.subscription))
+			return false;
+		return true;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Subscription getSubscription() {
+		return subscription;
+	}
+
+	public void setSubscription(Subscription subscription) {
+		this.subscription = subscription;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	
 
 }
