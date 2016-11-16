d6cea8ca50311ffd8c4217788f3a1e9329c80362
Nicolas Soriano <nico.sor@free.fr>
2016-11-16 12:20:16 +0100

    corrections

--- src/main/java/fr/dta/mediatic/media/model/Media.java
+++ src/main/java/fr/dta/mediatic/media/model/Media.java
@@ -1,5 +1,15 @@
 package fr.dta.mediatic.media.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Media")
 public class Media {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Title
+	@
 }
