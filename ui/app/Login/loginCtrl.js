12762d7069566d789c809ed5dd54668a20378b97
Pierre Robbes <robbesp@gmail.com>
2016-12-02 14:51:04 +0100

    modif de login

---
+++ ui/app/Login/loginCtrl.js
@@ -0,0 +1,16 @@
+var log = angular.module('myLog', []);
+
+/**log.config(function($routeProvider)
+{
+	$routeProvider.when('/Login',
+			{
+				templateUrl : './Login/login.html',
+				controller : 'LoginController',
+				controllerAs : 'LogCtrl'
+			});
+});**/
+
+log.controller('LoginController', function()
+{
+	
+});
