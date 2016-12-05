78780d47c6dabe13ab8b00c40468a0fe53893bca
Nicolas Christ <nicolas.christ25@gmail.com>
2016-12-05 10:23:25 +0100

    mise à jour des fichiers adherentSearch et adherentSearchController

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute', 'global', 'mediaSearch'])
+angular.module('app', ['ngRoute', 'global', 'mediaSearch', 'adherentSearch'])
 	.config(function($routeProvider){
 		$routeProvider.otherwise({
 			redirectTo:'/login'
