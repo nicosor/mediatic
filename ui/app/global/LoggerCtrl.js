4aac5a883fd1638861ebba92ef26d0ec964c5615
Pierre Robbes <robbesp@gmail.com>
2016-12-05 13:49:46 +0100

    affichage du menu et debut de media

--- ui/app/global/LoggerCtrl.js
+++ ui/app/global/LoggerCtrl.js
@@ -1,8 +1,19 @@
 angular.module('global').controller('LoggerCtrl', function($scope, loginService, $location){
 
-	if(loginService.isConnected()){
+	if(loginService.isConnected())
+	{
+		console.log('lgct '+loginService.isConnected());
 		$scope.showMenu=loginService.isConnected();
-	} else {
+	}
+	else
+	{
+		console.log('lgcf '+loginService.isConnected());
 		$location.url('/login');
+	}
+	
+	this.disconnect = function()
+	{
+		console.log('deconnexion');
+		loginService.disconnect();
 	}
 });	
