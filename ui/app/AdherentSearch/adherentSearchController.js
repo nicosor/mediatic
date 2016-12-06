4100921ee231b57ccc9bd789ef2768f7a9c9304d
Nicolas Christ <nicolas.christ25@gmail.com>
2016-12-06 10:47:57 +0100

    création du fichier 'adherentSearchModal' qui permet d'afficher la popup d'ajout d'adhérent

--- ui/app/AdherentSearch/adherentSearchController.js
+++ ui/app/AdherentSearch/adherentSearchController.js
@@ -1,5 +1,5 @@
 angular
-		.module('adherentSearch', ['ngRoute', 'services'])
+		.module('adherentSearch', ['ngRoute', 'services', 'ngSanitize', 'ui.bootstrap'])
 		.config(function($routeProvider) {
 			$routeProvider.when('/adherentSearch', {
 				templateUrl : './AdherentSearch/adherentSearch.html',
@@ -7,7 +7,7 @@ angular
 				controllerAs : 'adherentSrchCtrl'
 			});
 		})
-		.controller('AdherentSearchController', function($http, getAdherent) {
+		.controller('AdherentSearchController', function($http, getAdherent, $uibModal, $log, $document) {
 			var adh = this;
 			var url ='http://192.168.1.93:8090/resource/adherent.recherche'
 			getAdherent.getAdherentList(url).then(function (liste) {
@@ -25,4 +25,112 @@ angular
 			adh.checkCotisation = function(item){
 				return item.cotisation.fin > new Date();
 			};
-		});
+			
+			adh.animationEnabled = true;
+			
+			adh.open = function(size, parentSelector){
+				var parentElem = parentSelector ? angular.element($document[0]
+						.querySelector('.modal-demo ' + parentSelector))
+						: undefined;
+				var modalInstance = $uibModal.open({
+					animation : adh.animationEnabled,
+					arialLabelledBy : 'modal-title',
+					arialDescribedBy : 'modal-body',
+					templateUrl : 'AdherentSearch/adherentSearchModal.html',
+					controller : 'ModalInstanceCtrl',
+					controllerAs : '$ctrl',
+					size : size,
+					appendTo : parentElem,
+					resolve : {
+						items : function() {
+							return med.items;
+						}
+					}
+				});
+				
+				modalInstance.result.then(function(selectItem){
+					$ctrl.selected = selectedItem;
+				}, function(){
+					$log.info('Modal dismissed at: ' + new Date());
+				});
+			};
+			
+			adh.openComponentModal = function() {
+				var modalInstance = $uibModal.open({
+					animation : $ctrl.animationEnabled,
+					component : 'modalComponent',
+					resolve : {
+						items : function(){
+							return $ctrl.items;
+						}
+					}
+				});
+				
+				modalInstance.result.then(function(selectedItem) {
+					$ctrl.selected = selectedItem;
+				}, function() {
+					$log.info('modal-component dismissed at: ' + new Date());
+				});
+			};
+
+			adh.openMultipleModals = function() {
+				$uibModal.open({
+					animation : $ctrl.animationsEnabled,
+					ariaLabelledBy : 'modal-title-bottom',
+					ariaDescribedBy : 'modal-body-bottom',
+					templateUrl : 'stackedModal.html',
+					size : 'sm',
+					controller : function($scope) {
+						$scope.name = 'bottom';
+					}
+				});
+
+				$uibModal.open({
+					animation : $ctrl.animationsEnabled,
+					ariaLabelledBy : 'modal-title-top',
+					ariaDescribedBy : 'modal-body-top',
+					templateUrl : 'stackedModal.html',
+					size : 'sm',
+					controller : function($scope) {
+						$scope.name = 'top';
+					}
+				});
+			};
+
+			adh.toggleAnimation = function() {
+				$ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+			};
+		}).controller('ModalInstanceCtrl', function($uibModalInstance, items) {
+	var $ctrl = this;
+
+	
+}).component('modalComponent', {
+	templateUrl : 'searchAdherentModal',
+	bindings : {
+		resolve : '<',
+		close : '&',
+		dismiss : '&'
+	},
+	controller : function() {
+		var $ctrl = this;
+
+		$ctrl.$onInit = function() {
+			$ctrl.items = $ctrl.resolve.items;
+			$ctrl.selected = {
+				item : $ctrl.items[0]
+			};
+		};
+
+		$ctrl.ok = function() {
+			$ctrl.close({
+				$value : $ctrl.selected.item
+			});
+		};
+
+		$ctrl.cancel = function() {
+			$ctrl.dismiss({
+				$value : 'cancel'
+			});
+		};
+	}
+});
