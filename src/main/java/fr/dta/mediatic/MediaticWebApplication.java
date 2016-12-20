80ca250321a235dc33c9f05369a1fc918e06bfc9
Pierre Robbes <robbesp@gmail.com>
2016-12-20 14:43:24 +0100

    suppression de spring-boot dans le pom et les classes qui en dépendaient

--- src/main/java/fr/dta/mediatic/MediaticWebApplication.java
+++ src/main/java/fr/dta/mediatic/MediaticWebApplication.java
@@ -1,13 +1,5 @@
 package fr.dta.mediatic;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-public class MediaticWebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MediaticApplication.class);
-    }
+public class MediaticWebApplication {
 
 }
