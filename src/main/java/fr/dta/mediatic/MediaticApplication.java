23de631bba3f182e6ce919e94cf3ed0249f88201
Pierre Robbes <robbesp@gmail.com>
2016-12-20 12:16:08 +0100

    création du controller

--- src/main/java/fr/dta/mediatic/MediaticApplication.java
+++ src/main/java/fr/dta/mediatic/MediaticApplication.java
@@ -3,13 +3,30 @@ package fr.dta.mediatic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import fr.dta.mediatic.media.model.Media;
 
 @SpringBootApplication
 @ComponentScan("fr.dta.mediatic")
-public class MediaticApplication {
+@PropertySource("classpath:application.properties")
+public class MediaticApplication extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry)
+	{
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("ui/app/");
+		viewResolver.setSuffix(".html");
+		viewResolver.setExposeContextBeansAsAttributes(true);
+		registry.viewResolver(viewResolver);
+	}
+	
     public static void main1(String[] args) {
         SpringApplication.run(MediaticApplication.class, args);
         System.out.println("coucou");
---
+++ src/main/java/fr/dta/mediatic/controller/MediaticController.java
@@ -0,0 +1,52 @@
+package fr.dta.mediatic.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MediaticController
+{
+	@Autowired private UserDao userDao;
+	@Autowired private MediaDao mediaDao;
+	@Autowired private AdherentrDao adherentDao;
+	public static final String CURRENT_USER = "CURRENT_USER";
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String authentification(@RequestParam("login") String login, @RequestParam("password") String password, HttpServletRequest request)
+	{
+		User user = userDao.findUserByLogin(login);
+		if (user == null)
+		{
+			return "redirect:Login/login";
+		}
+		request.getSession().setAttribute(CURRENT_USER, user);
+		
+		return "redirect:MediaSearch/mediaSearch";
+	}
+	@RequestMapping(value = "/mediaSearch", method = RequestMethod.POST)
+	public String rechercheMedia(HttpServletRequest request)
+	{
+		User user = (User) request.getSession().getAttribute(CURRENT_USER);
+	}
+	@RequestMapping(value = "/adherantSearch", method = RequestMethod.POST)
+	public String rechercheAdherant(HttpServletRequest request)
+	{
+		User user = (User) request.getSession().getAttribute(CURRENT_USER);
+	}
+	@RequestMapping(value = "/media", method = RequestMethod.POST)
+	public String ficheMedia(HttpServletRequest request)
+	{
+		User user = (User) request.getSession().getAttribute(CURRENT_USER);
+	}
+	@RequestMapping(value = "/adherant", method = RequestMethod.POST)
+	public String ficheAdherant(HttpServletRequest request)
+	{
+		User user = (User) request.getSession().getAttribute(CURRENT_USER);
+	}
+}
