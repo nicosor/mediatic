5ed6861e905dc21052b7d0de21261966ff556544
Pierre Robbes <robbesp@gmail.com>
2016-12-22 14:48:08 +0100

    correction cohérence entre entities et database

--- src/main/java/fr/dta/mediatic/adherent/Adherent.java
+++ src/main/java/fr/dta/mediatic/adherent/Adherent.java
@@ -14,14 +14,11 @@ import fr.dta.mediatic.subscription.Subscription;
 @Entity
 public class Adherent extends AbstractModel
 {
-	@Column(name = "last_name")
 	private String lastname;
-	@Column(name = "first_name")
 	private String firstname;
 	@Temporal(TemporalType.DATE)
 	private Date birthday;
 	private String address;
-	@Column(name = "postal_code")
 	private int postalCode;
 	private String city;
 	@OneToOne
