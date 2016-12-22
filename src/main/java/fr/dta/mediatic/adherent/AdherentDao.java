d2546cd1a1425a52791a99208a6c020ccc1e82ff
Nicolas Christ <nicolas.christ25@gmail.com>
2016-12-22 15:55:28 +0100

    création des requêtes des daos Loan et subscription

--- src/main/java/fr/dta/mediatic/adherent/AdherentDao.java
+++ src/main/java/fr/dta/mediatic/adherent/AdherentDao.java
@@ -1,15 +1,9 @@
 package fr.dta.mediatic.adherent;
 
-import java.util.List;
-
-import javax.persistence.TypedQuery;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.dta.mediatic.abstracts.AbstractDao;
-import fr.dta.mediatic.loan.Loan;
-import fr.dta.mediatic.subscription.Subscription;
 
 @Repository
 @Transactional
@@ -19,24 +13,4 @@ public class AdherentDao extends AbstractDao<Adherent> {
 	protected Class<Adherent> getEntityClass() {
 		return null;
 	}
-	
-	public List<Loan> getLoanForThisAdh(Adherent adherent){
-		TypedQuery<Loan> query = super.getEntityManager().createQuery("FROM loan WHERE adherent =:adherent", Loan.class);
-		query.setParameter("adherent", adherent);
-		return query.getResultList();
-		
-	}
-	
-	public List<Subscription> getListSubscription(Adherent adherent){
-		TypedQuery<Subscription> query = super.getEntityManager().createQuery("from subscription where adherent =:adherent", Subscription.class);
-		query.setParameter("adherent", adherent);
-		return query.getResultList();
-	}
-	
-	public Object getLastSubscription(Adherent adherent){
-		TypedQuery<Subscription> query = super.getEntityManager().createQuery("from subscription where adherent =:adherent order by date_de_fin limit 1", Subscription.class);
-		query.setParameter("adherent", adherent);
-		return query.getResultList();
-	}
-	
 }
--- src/main/java/fr/dta/mediatic/subscription/SubscriptionDao.java
+++ src/main/java/fr/dta/mediatic/subscription/SubscriptionDao.java
@@ -1,5 +1,9 @@
 package fr.dta.mediatic.subscription;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import fr.dta.mediatic.abstracts.AbstractDao;
 
 public class SubscriptionDao extends AbstractDao<Subscription>{
@@ -7,5 +11,24 @@ public class SubscriptionDao extends AbstractDao<Subscription>{
 	@Override
 	protected Class<Subscription> getEntityClass() {
 		return null;
+	}
+	
+	public List<Subscription> getListSubscription(Long id){
+		TypedQuery<Subscription> query = super.getEntityManager().createQuery("select subscription.id, price, subscription_date, return_date "
+				+ "from subscription "
+				+ "join adherent on subscription.id = subscription_id "
+				+ "where adherent.id = adherent_id", Subscription.class);
+		query.setParameter("adherent_id", id);
+		return query.getResultList();
+	}
+	
+	public Object getLastSubscription(Long id){
+		TypedQuery<Subscription> query = super.getEntityManager().createQuery("select subscription.id, price, subscription_date, return_date "
+				+ "from subscription "
+				+ "join adherent on subscription.id = subscription_id "
+				+ "where adherent.id = adherent_id"
+				+ "order by return_date desc", Subscription.class);
+		query.setParameter("adherent_id", id);
+		return query.setMaxResults(1).getResultList();
 	}
 }
