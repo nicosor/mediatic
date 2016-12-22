78af30dd5ceb673baa21db1ec7fb7c337a3d7aae
Nicolas Soriano <nico.sor@free.fr>
2016-12-22 15:53:42 +0100

    ajout du web initializer et du gif pour l'image de fond

--- src/main/java/fr/dta/mediatic/MediaticApplication.java
+++ src/main/java/fr/dta/mediatic/MediaticApplication.java
@@ -1,28 +1,5 @@
 package fr.dta.mediatic;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+public class MediaticApplication  {
 
-@ComponentScan("fr.dta.mediatic")
-@PropertySource("classpath:application.properties")
-public class MediaticApplication extends WebMvcConfigurerAdapter {
-
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry)
-	{
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("ui/app/");
-		viewResolver.setSuffix(".html");
-		viewResolver.setExposeContextBeansAsAttributes(true);
-		registry.viewResolver(viewResolver);
-	}
-	
-    public static void main1(String[] args) {
-        System.out.println("coucou");
-    }
 }
